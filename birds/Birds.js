63b7c2a15001d18b8b9fd08030321ca10264e18d
DUZAKH <126305138+DUZAKH@users.noreply.github.com>
2025-02-15 19:56:20 -0500

    Update Birds.js

--- birds/Birds.js
+++ birds/Birds.js
@@ -1,9 +1,15 @@
+//Sean, Daniel and Kiana
 class Bird {
-    constructor(x, y,) {
+    constructor(x) {
         // We write instructions to set up a Flower here
         // Position and size information
         this.x = x;
         this.y = 140;
+        this.w = 92;
+        this.h = 90;
+        this.v = 3.5;
+        this.aniRef = null;
+        this.skyBound = document.querySelector(".sky").getBoundingClientRect();
 
         // Color information
 
@@ -18,8 +24,22 @@ class Bird {
         this.image.classList.add("Bird");
         this.image.style.left = this.x + "px";
         this.image.style.top = this.y + "px";
+        this.image.style.width = this.w + "px";
+        this.image.style.height = this.h + "px";
+        this.image.style.position = "absolute";
         //add to the DOM
-        document.querySelector(".wire").appendChild(this.image);
+        document.querySelector(".sky").appendChild(this.image);
+        this.aniRef = window.requestAnimationFrame(() => this.animateBird());
+    }
+
+    animateBird() {
+        this.y += this.v;
+        console.log(this.y)
+        if (this.y > this.skyBound.height - this.h || this.y < 0) {
+            this.v = -this.v;
+        }
+        this.image.style.top = this.y + "px";
+        this.aniRef = window.requestAnimationFrame(() => this.animateBird());
     }
 
 }
